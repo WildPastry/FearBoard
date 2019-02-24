ee6a726a2a78da20be9b073526e49f4770b161e9
WildPastry <mykdsn@gmail.com>
2019-02-24 19:25:29 +1300

    Updated files

---
+++ src/js/charts.js
@@ -0,0 +1,343 @@
+// GLOBAL VARIABLES
+var classData;
+var mapKey;
+
+// GET MAP KEY
+$.ajax({
+  url: '../config.json',
+  dataType: 'json',
+  type: 'get',
+  success: function (keys) {
+    mapKey = keys[0].MAP_KEY;
+    console.log("Key loaded...");
+    getData();
+  },
+  error: function (error) {
+    console.log(error);
+    console.log("Can't load the key...");
+  }
+});
+
+// CHARTS
+// LOAD PACKAGES AND MAP KEY
+google.charts.load('current', {
+  'packages': ['corechart', 'bar', 'geochart'],
+  'mapsApiKey': mapKey
+});
+
+// LOAD CHARTS
+google.charts.setOnLoadCallback(drawPieChart);
+google.charts.setOnLoadCallback(drawRegionsMap);
+google.charts.setOnLoadCallback(drawColumnChart);
+google.charts.setOnLoadCallback(drawBarChart);
+
+// GET GRAPH DATA
+function getData() {
+  $.ajax({
+    url: 'json/classData.json',
+    dataType: 'json',
+    type: 'get',
+    success: function (classData) {
+      console.log('Complete class data:');
+      for (var i = 0; i < classData.length; i++) {
+        console.log(classData[i].age, classData[i].sex, classData[i].country, classData[i].creature, classData[i].amount, classData[i].item);
+      }
+    },
+    error: function (error) {
+      console.log(error);
+      console.log("can't load class data...");
+    }
+  });
+}
+
+// PIECHART
+function drawPieChart() {
+  $.ajax({
+    url: 'json/classData.json',
+    dataType: 'json',
+    type: 'get',
+    success: function (classData) {
+      var data = new google.visualization.DataTable();
+      data.addColumn('string', 'Creature');
+      data.addColumn('number', 'Votes');
+      var mouse = 0;
+      var spider = 0;
+      var shark = 0;
+      var bear = 0;
+      var snake = 0;
+      for (var i = 0; i < classData.length; i++) {
+        if (classData[i].creature == "Mouse") mouse++;
+        if (classData[i].creature == "Spider") spider++;
+        if (classData[i].creature == "Shark") shark++;
+        if (classData[i].creature == "Bear") bear++;
+        if (classData[i].creature == "Snake") snake++;
+      };
+      console.log("Pie chart data loaded...");
+      console.log(mouse, spider, shark, bear, snake);
+      data.addRow(['Mouse', mouse]);
+      data.addRow(['Spider', spider]);
+      data.addRow(['Shark', shark]);
+      data.addRow(['Bear', bear]);
+      data.addRow(['Snake', snake]);
+
+      var options = {
+        tooltip: {
+          textStyle: {
+            color: '#03223d'
+          },
+          showColorCode: true
+        },
+        width: 1000,
+        height: 500,
+        fontSize: 17,
+        fontName: 'Source Sans Pro',
+        title: ' ',
+        backgroundColor: {
+          fill: 'transparent'
+        },
+        colors: ['#4182bb', '#005daf', '#f7697c', '#004581', '#e998b0'],
+        titleTextStyle: {
+          color: '#fff'
+        },
+        legend: {
+          textStyle: {
+            color: 'fff'
+          },
+        },
+        'pieHole': 0.4,
+        is3D: true
+      };
+      var chart = new google.visualization.PieChart(document.getElementById('chart1'));
+      chart.draw(data, options);
+    },
+    error: function (error) {
+      console.log(error);
+      console.log("can't load pie chart data...");
+    }
+  });
+}
+
+// GEOCHART
+function drawRegionsMap() {
+  $.ajax({
+    url: 'json/classData.json',
+    dataType: 'json',
+    type: 'get',
+    success: function (classData) {
+      var data = new google.visualization.DataTable();
+      data.addColumn('string', 'Country');
+      data.addColumn('number', 'Votes');
+      var china = 0;
+      var us = 0;
+      var philippines = 0;
+      var africa = 0;
+      var syria = 0;
+      var bulgaria = 0;
+      var turkey = 0;
+      for (var i = 0; i < classData.length; i++) {
+        if (classData[i].country == "China") china++;
+        if (classData[i].country == "United States") us++;
+        if (classData[i].country == "Philippines") philippines++;
+        if (classData[i].country == "Africa") africa++;
+        if (classData[i].country == "Syria") syria++;
+        if (classData[i].country == "Bulgaria") bulgaria++;
+        if (classData[i].country == "Turkey") turkey++;
+      };
+      console.log("Geo chart data loaded...");
+      console.log(china, us, philippines, africa, syria, bulgaria, turkey);
+      data.addRow(['China', china]);
+      data.addRow(['United States', us]);
+      data.addRow(['Philippines', philippines]);
+      data.addRow(['Africa', africa]);
+      data.addRow(['Syria', syria]);
+      data.addRow(['Bulgaria', bulgaria]);
+      data.addRow(['Turkey', turkey]);
+
+      var options = {
+        tooltip: {
+          textStyle: {
+            color: '#03223d'
+          },
+          showColorCode: true
+        },
+        fontSize: 17,
+        fontName: 'Source Sans Pro',
+        stroke: 'none',
+        width: 1000,
+        height: 500,
+        title: ' ',
+        backgroundColor: {
+          fill: 'transparent'
+        },
+        colors: ['#b12199', '#f7697c'],
+        titleTextStyle: {
+          color: '#fff'
+        },
+        datalessRegionColor: '#b9d3f0',
+        legend: {
+          position: 'left',
+          textStyle: {
+            color: 'fff',
+          }
+        }
+      };
+      var chart = new google.visualization.GeoChart(document.getElementById('chart2'));
+      chart.draw(data, options);
+    },
+    error: function (error) {
+      console.log(error);
+      console.log("can't load geo chart data...");
+    }
+  });
+}
+
+// COLUMN CHART
+function drawColumnChart() {
+  $.ajax({
+    url: 'json/classData.json',
+    dataType: 'json',
+    type: 'get',
+    success: function (classData) {
+      var data = new google.visualization.DataTable();
+      data.addColumn('string', 'Creature');
+      data.addColumn('number', 'Cash');
+      console.log('Column chart data loaded...');
+      for (var i = 0; i < classData.length; i++) {
+        console.log(classData[i].name, classData[i].amount);
+        data.addRow([classData[i].name, classData[i].amount]);
+      }
+      var options = {
+        tooltip: {
+          textStyle: {
+            color: '#03223d'
+          },
+          showColorCode: true
+        },
+        fontSize: 17,
+        fontName: 'Source Sans Pro',
+        width: 1000,
+        height: 500,
+        title: ' ',
+        backgroundColor: {
+          fill: 'transparent'
+        },
+        colors: ['#e998b0'],
+        titleTextStyle: {
+          color: '#fff'
+        },
+        legend: {
+          position: 'none',
+          textStyle: {
+            color: 'fff'
+          },
+        },
+        vAxis: {
+          textStyle: {
+            color: '#fff'
+          },
+          gridlines: {
+            color: '#004581'
+          }
+        },
+        hAxis: {
+          textStyle: {
+            color: '#fff'
+          },
+          gridlines: {
+            color: '#004581'
+          }
+        }
+      }
+
+      var chart = new google.visualization.ColumnChart(document.getElementById('chart3'));
+      chart.draw(data, options);
+    },
+    error: function (error) {
+      console.log(error);
+      console.log("Can't load column chart data...");
+    }
+  });
+}
+
+// BAR CHART
+function drawBarChart() {
+  $.ajax({
+    url: 'json/classData.json',
+    dataType: 'json',
+    type: 'get',
+    success: function (classData) {
+      var data = new google.visualization.DataTable();
+      data.addColumn('string', 'Creature');
+      data.addColumn('number', 'Votes');
+      var nachos = 0;
+      var public_speaking = 0;
+      var heights = 0;
+      var swimming = 0;
+      var dentist = 0;
+      for (var i = 0; i < classData.length; i++) {
+        if (classData[i].item == "Nachos") nachos++;
+        if (classData[i].item == "Public Speaking") public_speaking++;
+        if (classData[i].item == "Heights") heights++;
+        if (classData[i].item == "Swimming") swimming++;
+        if (classData[i].item == "Dentist") dentist++;
+      };
+      console.log("Bar chart data loaded...");
+      console.log(nachos, public_speaking, heights, swimming, dentist);
+      data.addRow(['Nachos', nachos]);
+      data.addRow(['Public Speaking', public_speaking]);
+      data.addRow(['Heights', heights]);
+      data.addRow(['Swimming', swimming]);
+      data.addRow(['Dentist', dentist]);
+
+      var options = {
+        tooltip: {
+          textStyle: {
+            color: '#03223d'
+          },
+          showColorCode: true
+        },
+        fontSize: 17,
+        fontName: 'Source Sans Pro',
+        width: 1000,
+        height: 500,
+        title: ' ',
+        backgroundColor: {
+          fill: 'transparent'
+        },
+        legend: {
+          position: 'none',
+          textStyle: {
+            color: 'fff'
+          }
+        },
+        colors: ['#cb98e9'],
+        titleTextStyle: {
+          color: '#fff'
+        },
+        vAxis: {
+          textStyle: {
+            color: '#fff'
+          },
+          gridlines: {
+            color: '#004581'
+          }
+        },
+        hAxis: {
+          textStyle: {
+            color: '#fff'
+          },
+          gridlines: {
+            color: '#004581'
+          }
+        }
+      }
+
+      var chart = new google.visualization.BarChart(document.getElementById('chart4'));
+      chart.draw(data, options);
+    },
+    error: function (error) {
+      console.log(error);
+      console.log("Can't load bar chart data...");
+    }
+  });
+}
--- src/js/main.js
+++ src/js/main.js
@@ -1,327 +1,20 @@
 /*jslint browser:true */
 console.log('Javascript connected...');
 
-// GLOBAL VARIABLES
-var classData;
-var mapKey;
-
 // EVENT LISTENERS
 // document.getElementById('section2').addEventListener('click', section1);
-document.getElementById('spooder').addEventListener('click', section2);
-
-// GET MAP KEY
-$.ajax({
-  url: '../config.json',
-  dataType: 'json',
-  type: 'get',
-  success: function (keys) {
-    mapKey = keys[0].MAP_KEY;
-    console.log("Key loaded...");
-    getData();
-  },
-  error: function (error) {
-    console.log(error);
-    console.log("Can't load the key...");
-  }
-});
-
-// CHARTS
-// LOAD PACKAGES
-google.charts.load('current', {
-  'packages': ['corechart', 'bar', 'geochart'],
-  'mapsApiKey': mapKey
-});
-
-// LOAD CHARTS
-google.charts.setOnLoadCallback(drawPieChart);
-google.charts.setOnLoadCallback(drawRegionsMap);
-google.charts.setOnLoadCallback(drawScatterChart);
-google.charts.setOnLoadCallback(drawBarChart);
-
-// GET DATA
-function getData() {
-  $.ajax({
-    url: 'json/classData.json',
-    dataType: 'json',
-    type: 'get',
-    success: function (classData) {
-      console.log('Class data:');
-      for (var i = 0; i < classData.length; i++) {
-        console.log(classData[i].age, classData[i].sex, classData[i].country, classData[i].nachos, classData[i].public_speaking, classData[i].heights, classData[i].swimming, classData[i].dentist, classData[i].creature, classData[i].amount);
-      }
-    },
-    error: function (error) {
-      console.log(error);
-      console.log("can't load class data...");
-    }
-  });
-}
-
-// PIECHART
-function drawPieChart() {
-  $.ajax({
-    url: 'json/classData.json',
-    dataType: 'json',
-    type: 'get',
-    success: function (classData) {
-      console.log('Pie chart data:');
-      var data = new google.visualization.DataTable();
-      data.addColumn('string', 'Creature');
-      data.addColumn('number', 'Age');
-      for (var i = 0; i < classData.length; i++) {
-        console.log(classData[i].creature, classData[i].age);
-        data.addRow([classData[i].creature, classData[i].age]);
-      }
-      var options = {
-        height: 400,
-        width: 600,
-        title: 'Most terrifying creature',
-        colors: ['#e0440e', '#e6693e', '#ec8f6e', '#f3b49f', '#f6c7b6'],
-        pieHole: 0.4,
-      };
-      var chart = new google.visualization.PieChart(document.getElementById('graph1'));
-      chart.draw(data, options);
-    },
-    error: function (error) {
-      console.log(error);
-      console.log("can't load pie chart data...");
-    }
-  });
-
-  // var data = new google.visualization.DataTable();
-  // data.addColumn('number', 'Age');
-  // data.addColumn('string', 'Creature');
-  // for (var i = 0; i < classData.length; i++) {
-  //     console.log(classData[i].age, classData[i].creature);
-  //     data.addRow([classData[i].age, classData[i].creature])
-  // }
-
-  // var options = {
-  //   height: 400,
-  //   width: 600,
-  //   title: 'Graph 1',
-  //   colors: ['#e0440e', '#e6693e', '#ec8f6e', '#f3b49f', '#f6c7b6'],
-  //   pieHole: 0.4,
-  // };
-
-  // var chart = new google.visualization.PieChart(document.getElementById('graph1'));
-  // chart.draw(data, options);
-}
-
-// GEOCHART
-function drawRegionsMap() {
-  var data = google.visualization.arrayToDataTable([
-    ['Country', 'Popularity'],
-    ['Germany', 200],
-    ['United States', 300],
-    ['Brazil', 400],
-    ['Canada', 500],
-    ['France', 600],
-    ['RU', 700]
-  ]);
-
-  var options = {
-    height: 400,
-    width: 600,
-    title: 'Graph 2',
-    colors: ['#e0440e', '#e6693e', '#ec8f6e', '#f3b49f', '#f6c7b6'],
-  };
-
-  var chart = new google.visualization.GeoChart(document.getElementById('graph2'));
-
-  chart.draw(data, options);
-}
-
-// SCATTER CHART
-function drawScatterChart() {
-  $.ajax({
-    url: 'json/classData.json',
-    dataType: 'json',
-    type: 'get',
-    success: function (classData) {
-      console.log('Scatter chart data:');
-      var data = new google.visualization.DataTable();
-      data.addColumn('number', 'Age');
-      data.addColumn('number', 'Cash');
-      for (var i = 0; i < classData.length; i++) {
-        console.log(classData[i].age, classData[i].amount);
-        data.addRow([classData[i].age, classData[i].amount]);
-      }
-      var options = {
-        height: 400,
-        width: 600,
-        title: 'How much money to be trapped with your terrifying creature?',
-        hAxis: {
-          title: 'Age',
-          // minValue: 0,
-          // maxValue: 15
-        },
-        vAxis: {
-          title: 'Cash',
-          // minValue: 0,
-          // maxValue: 15
-        },
-        colors: ['#e0440e', '#e6693e', '#ec8f6e', '#f3b49f', '#f6c7b6'],
-      };
-      var chart = new google.visualization.ScatterChart(document.getElementById('graph3'));
-      chart.draw(data, options);
-    },
-    error: function (error) {
-      console.log(error);
-      console.log("can't load scatter chart data...");
-    }
-  });
-}
-
-// function drawScatterChart() {
-//   var data = google.visualization.arrayToDataTable([
-//     ['Age', 'Weight'],
-//     [8, 12],
-//     [4, 5.5],
-//     [11, 14],
-//     [4, 5],
-//     [3, 3.5],
-//     [6.5, 7]
-//   ]);
-
-//   var options = {
-//     height: 400,
-//     width: 600,
-//     title: 'Graph 3',
-//     hAxis: {
-//       title: 'Age',
-//       minValue: 0,
-//       maxValue: 15
-//     },
-//     vAxis: {
-//       title: 'Weight',
-//       minValue: 0,
-//       maxValue: 15
-//     },
-//     colors: ['#e0440e', '#e6693e', '#ec8f6e', '#f3b49f', '#f6c7b6'],
-//   };
-
-//   var chart = new google.visualization.ScatterChart(document.getElementById('graph3'));
-
-//   chart.draw(data, options);
-// }
-
-// BAR CHART
-// var data = google.visualization.arrayToDataTable([
-//   ['Year', 'Sales', 'Expenses', 'Profit'],
-//   ['2014', 1000, 400, 200],
-//   ['2015', 1170, 460, 250],
-//   ['2016', 660, 1120, 300],
-//   ['2017', 1030, 540, 350]
-// ]);
-
-function drawBarChart() {
-  $.ajax({
-    url: 'json/classData.json',
-    dataType: 'json',
-    type: 'get',
-    success: function (classData) {
-      console.log('Bar chart data:');
-      var data = new google.visualization.DataTable();
-      data.addColumn('string', 'Rank');
-      data.addColumn('number', 'Rank');
-      data.addColumn('number', 'Nachos');
-      for (var i = 0; i < classData.length; i++) {
-        console.log(classData[i].item, classData[i].rank);
-        data.addRow([classData[i].item, classData[i].rank, classData[i].age]);
-      }
-      var options = {
-        bars: 'vertical',
-        height: 400,
-        width: 600,
-        title: 'Items ranked in order of fear',
-    hAxis: {
-      title: 'Age',
-      // minValue: 0,
-    },
-    vAxis: {
-      title: 'Rank'
-    },
-        colors: ['#e0440e', '#e6693e', '#ec8f6e', '#f3b49f', '#f6c7b6'],
-      };
-      var chart = new google.visualization.BarChart(document.getElementById('graph4'));
-      chart.draw(data, options);
-    },
-    error: function (error) {
-      console.log(error);
-      console.log("can't load bar chart data...");
-    }
-  });
-}
-// function drawBarChart() {
-//   var data = google.visualization.arrayToDataTable([
-//     ['City', '2010 Population', '2000 Population'],
-//     ['New York City, NY', 8175000, 8008000],
-//     ['Los Angeles, CA', 3792000, 3694000],
-//     ['Chicago, IL', 2695000, 2896000],
-//     ['Houston, TX', 2099000, 1953000],
-//     ['Philadelphia, PA', 1526000, 1517000]
-//   ]);
-
-//   data = google.visualization.arrayToDataTable([
-//     ['City', '2010 Population', {
-//         type: 'string',
-//         role: 'annotation'
-//       },
-//       '2000 Population', {
-//         type: 'string',
-//         role: 'annotation'
-//       }
-//     ],
-//     ['New York City, NY', 8175000, '8.1M', 8008000, '8M'],
-//     ['Los Angeles, CA', 3792000, '3.8M', 3694000, '3.7M'],
-//     ['Chicago, IL', 2695000, '2.7M', 2896000, '2.9M'],
-//     ['Houston, TX', 2099000, '2.1M', 1953000, '2.0M'],
-//     ['Philadelphia, PA', 1526000, '1.5M', 1517000, '1.5M']
-//   ]);
-
-//   var options = {
-//     height: 400,
-//     width: 600,
-//     title: 'Graph 4',
-//     colors: ['#e0440e', '#e6693e', '#ec8f6e', '#f3b49f', '#f6c7b6'],
-//     chartArea: {
-//       width: '50%'
-//     },
-//     annotations: {
-//       alwaysOutside: true,
-//       textStyle: {
-//         fontSize: 12,
-//         auraColor: 'none',
-//         color: '#555'
-//       },
-//       boxStyle: {
-//         stroke: '#ccc',
-//         strokeWidth: 1,
-//         gradient: {
-//           color1: '#e0440e',
-//           color2: '#e6693e',
-//           x1: '0%',
-//           y1: '0%',
-//           x2: '100%',
-//           y2: '100%'
-//         }
-//       }
-//     },
-//     hAxis: {
-//       title: 'Total Population',
-//       minValue: 0,
-//     },
-//     vAxis: {
-//       title: 'City'
-//     }
-//   };
-//   var chart = new google.visualization.BarChart(document.getElementById('graph4'));
-//   chart.draw(data, options);
-// }
+document.getElementById('spooder').addEventListener('click', show2from1);
+document.getElementById('show1from2').addEventListener('click', show1from2);
+document.getElementById('show2from3').addEventListener('click', show2from3);
+document.getElementById('show3from2').addEventListener('click', show3from2);
+document.getElementById('show3from4').addEventListener('click', show3from4);
+document.getElementById('show4from3').addEventListener('click', show4from3);
+document.getElementById('show4from5').addEventListener('click', show4from5);
+document.getElementById('show1from5').addEventListener('click', show1from5);
+document.getElementById('show5from4').addEventListener('click', show5from4);
 
 // PAGE ANIMATION
-function section1() {
+function show1from2() {
   $(function () {
     $("#section1").show();
   });
@@ -330,7 +23,16 @@ function section1() {
   });
 }
 
-function section2() {
+function show1from5() {
+  $(function () {
+    $("#section1").show();
+  });
+  $(function () {
+    $("#section5").hide();
+  });
+}
+
+function show2from1() {
   $(function () {
     $("#section2").show();
   });
@@ -338,3 +40,63 @@ function section2() {
     $("#section1").hide();
   });
 }
+
+function show2from3() {
+  $(function () {
+    $("#section2").show();
+  });
+  $(function () {
+    $("#section3").hide();
+  });
+}
+
+function show3from2() {
+  $(function () {
+    $("#section3").show();
+  });
+  $(function () {
+    $("#section2").hide();
+  });
+}
+
+function show3from4() {
+  $(function () {
+    $("#section3").show();
+  });
+  $(function () {
+    $("#section4").hide();
+  });
+}
+
+function show4from3() {
+  $(function () {
+    $("#section4").show();
+  });
+  $(function () {
+    $("#section3").hide();
+  });
+}
+
+function show4from5() {
+  $(function () {
+    $("#section4").show();
+  });
+  $(function () {
+    $("#section5").hide();
+  });
+}
+
+function show5from4() {
+  $(function () {
+    $("#section5").show();
+  });
+  $(function () {
+    $("#section4").hide();
+  });
+}
+
+// ANIMSITION
+$(".animsition").animsition({
+  inDuration: 800,
+  outDuration: 800
+});
